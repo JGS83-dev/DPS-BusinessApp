8dfbf0db38a78cd3f696414026cac8187a0fde7e
birdkron <js8215001@gmail.com>
2023-09-18 17:35:30 -0600

    Agregada data de ejemplo

---
+++ business-app/data/empresas.js
@@ -0,0 +1,30 @@
+export const empresas = [
+    {
+        id: 1,
+        nombre: 'Pupuseria la Esquina',
+        descripcion: 'Pupuseria la Esquina',
+        imagen: '',
+        Ubicacion: '',
+        Sucursales: ['', ''],
+        Horario: ['', '', ''],
+        Telefono: ['', '', ''],
+        fechaFundacion: '',
+        fechaRegistro: '',
+        codigo: '',
+        estado: ''
+    },
+    {
+        id: 2,
+        nombre: 'Llanteria el paso',
+        descripcion: 'Llanteria el paso',
+        imagen: '',
+        Ubicacion: '',
+        Sucursales: ['', ''],
+        Horario: ['', '', ''],
+        Telefono: ['', '', ''],
+        fechaFundacion: '',
+        fechaRegistro: '',
+        codigo: '',
+        estado: ''
+    }
+]
---
+++ business-app/data/eventos.js
@@ -0,0 +1,18 @@
+export const eventos = [
+    {
+        titulo: 'Nuevas promociones',
+        descripcion: {
+            resumen: '',
+            completa: '',
+            otros: ''
+        },
+        imagenes: {
+            principal: '',
+            otras: ['', ''],
+        },
+        fechaInicio: '',
+        fechaFin: '',
+        estado: '',
+        autor: 1
+    }
+]
