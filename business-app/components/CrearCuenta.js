a57f25a656373148fb6b0e142873ce0101f848ac
birdkron <js8215001@gmail.com>
2023-09-20 16:02:40 -0600

    Base crear cuenta

--- business-app/components/CrearCuenta.js
+++ business-app/components/CrearCuenta.js
@@ -1,2 +1,32 @@
-export const CrearCuenta = {
+import React from 'react';
+import ContenedorPrincipal from './ContenedorPrincipal';
+import {
+    Text,
+    StyleSheet
+} from 'react-native';
+import { colores } from '../config/colores';
+
+const CrearCuenta = ({ navigation }) => {
+    return (
+
+        <ContenedorPrincipal titulo="Crear Cuenta"
+            navigation={navigation}
+            contenido={(
+                <>
+                    <Text style={styles.letra}>Formulario para crear cuenta</Text>
+                </>
+            )}></ContenedorPrincipal>
+
+    );
 }
+
+export default CrearCuenta
+
+const styles = StyleSheet.create({
+    letra: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: colores.letra
+    }
+});
+
