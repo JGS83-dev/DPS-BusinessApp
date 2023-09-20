0a757bacd21655d5c95cac8a1c0122b79150c152
birdkron <js8215001@gmail.com>
2023-09-19 19:32:02 -0600

    Firebase persist fix

--- business-app/config/firebase/FirebaseConfig.js
+++ business-app/config/firebase/FirebaseConfig.js
@@ -1,9 +1,12 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, getReactNativePersistence } from "firebase/auth";
 import { firebaseConfig } from "./ServiceAccount";
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+const auth = getAuth(app, {
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
 const createAccount = createUserWithEmailAndPassword(app)
 
-export { auth,createAccount };
+export { auth, createAccount };
