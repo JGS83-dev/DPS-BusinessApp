1159c4f04108f5bb495f03533eaa36d24bf876ed
birdkron <js8215001@gmail.com>
2023-09-19 19:48:05 -0600

    Agregado ignorar logs de advertencia

--- business-app/App.js
+++ business-app/App.js
@@ -3,8 +3,13 @@ import Inicio from './components/Inicio';
 import Login from './components/Login';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {
+LogBox
+} from 'react-native';
 
 const Stack = createNativeStackNavigator();
+
+LogBox.ignoreAllLogs();
 
 function App() {
   return (
