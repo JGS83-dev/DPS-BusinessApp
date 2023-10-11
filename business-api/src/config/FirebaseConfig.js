ca4eafe54f7f4240371483e1c6584b7c6402abd4
birdkron <js8215001@gmail.com>
2023-10-11 11:49:28 -0600

    Config firebase

--- business-api/src/config/FirebaseConfig.js
+++ business-api/src/config/FirebaseConfig.js
@@ -1,11 +1,8 @@
-const { initializeApp, cert } = require('firebase-admin/app');
-const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
-const serviceAccount = require('./ServiceAccount.json');
+import { initializeApp, cert } from 'firebase-admin/app';
+import serviceAccount from './ServiceAccount.js';
 
-initializeApp({
+const firebase = initializeApp({
   credential: cert(serviceAccount)
 });
 
-const db = getFirestore();
-
-module.exports = { db } 
+export default firebase;
