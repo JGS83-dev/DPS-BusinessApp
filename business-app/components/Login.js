96c45bc21c48b30dda07ac06f454fc79ade4837b
birdkron <js8215001@gmail.com>
2023-10-14 19:29:35 -0600

    Implementado Login con Google

--- business-app/components/Login.js
+++ business-app/components/Login.js
@@ -1,30 +1,34 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, setState } from "react";
 import {
-    Text,
-    StyleSheet,
-    KeyboardAvoidingView,
-    View,
-    TextInput,
-    TouchableOpacity,
-    Dimensions
-} from 'react-native';
+  Text,
+  StyleSheet,
+  KeyboardAvoidingView,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Dimensions,
+  Alert,
+} from "react-native";
+import { WEB_CLIENT_ID, ANDROID_CLIENT_ID } from "@env";
+import {
+  GoogleSignin,
+  statusCodes,
+  GoogleSigninButton,
+} from "@react-native-google-signin/google-signin";
 
-import ContenedorPrincipal from './ContenedorPrincipal';
-import { colores } from '../config/colores';
-import { app } from '../config/firebase/FirebaseConfig';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faAddressCard, faImage } from '@fortawesome/free-solid-svg-icons';
+import ContenedorPrincipal from "./ContenedorPrincipal";
+import { colores } from "../config/colores";
+import { app } from "../config/firebase/FirebaseConfig";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faImage } from "@fortawesome/free-solid-svg-icons";
 import {
-    getAuth,
-    createUserWithEmailAndPassword,
-    getReactNativePersistence,
-    signInWithEmailAndPassword,
-    signInWithPopup,
-    GoogleAuthProvider
+  getAuth,
+  getReactNativePersistence,
+  signInWithEmailAndPassword,
 } from "firebase/auth";
-import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
-const screenHeight = Dimensions.get('window').height;
+const screenHeight = Dimensions.get("window").height;
 const cabeceraMensajeHeight = 0.07 * screenHeight;
 const cabeceraMensajePadding = 0.01 * screenHeight;
 const cabeceraMensajeMargin = 0.03 * screenHeight;
@@ -32,197 +36,208 @@ const iconSize = 0.2 * screenHeight;
 const iconMargin = 0.01 * screenHeight;
 
 function Login({ navigation }) {
-    const auth = getAuth(app, {
-        persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+  const auth = getAuth(app, {
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+  });
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [userInfo, setuserInfo] = useState();
+
+  // const navigation = useNavigation()
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        // navigation.navigate("Inicio")
+        signOut(auth)
+          .then(() => {
+            console.log("Cierre de sesión exitoso");
+          })
+          .catch((error) => {
+            console.log("Error al cerrar sesión");
+          });
+      }
     });
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    return unsubscribe;
+  }, []);
 
-    // const navigation = useNavigation()
-
-    useEffect(() => {
-        const unsubscribe = auth.onAuthStateChanged(user => {
-            if (user) {
-                // navigation.navigate("Inicio")
-                signOut(auth).then(() => {
-                    console.log('Cierre de sesión exitoso')
-                }).catch((error) => {
-                    console.log('Error al cerrar sesión')
-                });
-            }
-        })
-
-        return unsubscribe
-    }, [])
-
-    const handleProvider = () => {
-        console.log('Iniciando sesion con Google');
-        const provider = new GoogleAuthProvider(app);
-        signInWithPopup(auth, provider)
-            .then((result) => {
-                const credential = GoogleAuthProvider.credentialFromResult(result);
-                const token = credential.accessToken;
-                const user = result.user;
-                console.log('Sesión iniciada con Google:', user.email);
-                console.log('Token generado con Google:', token);
-            })
-            .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                const email = error.customData.email;
-                const credential = GoogleAuthProvider.credentialFromError(error);
-                console.log('Credenciales del fallo:', credential);
-            });
+  const handleProvider = async () => {
+    try {
+      console.log("Iniciando sesion con Google");
+      await GoogleSignin.hasPlayServices();
+      const data = await GoogleSignin.signIn();
+      setuserInfo(data);
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        alert("Operación cancelada por el usuario");
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        alert("Inicio de sesion en progreso");
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        alert("Servicio no disponible");
+      } else {
+        console.log("Error:", error);
+      }
     }
+  };
 
-    const handleSignUp = () => {
-        console.log('Creando Cuenta');
-        navigation.navigate("CrearCuenta");
-        // createUserWithEmailAndPassword(auth, email, password)
-        //     .then(userCredentials => {
-        //         const user = userCredentials.user;
-        //         console.log('Cuenta creada con:', user.email);
-        //     })
-        //     .catch(error => alert(error.message))
+  useEffect(() => {
+    // console.log("Configurando Google Services");
+    // console.log("WEB_CLIENT_ID:", WEB_CLIENT_ID);
+    GoogleSignin.configure({
+      scopes: ["email", "https://www.googleapis.com/auth/drive.readonly"],
+      webClientId: WEB_CLIENT_ID,
+      offlineAccess: true,
+    });
+  }, []);
+
+  useEffect(() => {
+    if (userInfo !== undefined) {
+    //   console.log("Info de sesión:", userInfo);
+      Alert.alert("Inicio de sesión", "Inicio de sesión con Google Exitoso", [
+        { text: "OK", onPress: () => navigation.navigate("Inicio") },
+      ]);
     }
+  }, [userInfo]);
 
-    const handleLogin = () => {
-        console.log('Iniciando sesion');
-        signInWithEmailAndPassword(auth, email, password)
-            .then(userCredentials => {
-                const user = userCredentials.user;
-                console.log('Sesion iniciada:', user.email);
-                navigation.navigate("Inicio")
-            })
-            .catch(error => alert(error.message))
-    }
+  const handleSignUp = () => {
+    console.log("Creando Cuenta");
+    navigation.navigate("CrearCuenta");
+  };
 
-    return (
-        <>
-            <ContenedorPrincipal titulo="Iniciar Sesión"
-                navigation={navigation}
-                contenido={(
-                    <>
-                        <View style={styles.cabeceraMensaje}>
-                            <Text style={styles.letraTitulo} >Complete los campos</Text>
-                        </View>
-                        <View style={styles.imgContainer}>
-                            <FontAwesomeIcon icon={faImage} size={iconSize} />
-                        </View>
+  const handleLogin = () => {
+    console.log("Iniciando sesion");
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredentials) => {
+        const user = userCredentials.user;
+        console.log("Sesion iniciada:", user.email);
+        navigation.navigate("Inicio");
+      })
+      .catch((error) => alert(error.message));
+  };
 
-                        <KeyboardAvoidingView
-                            style={styles.container}
-                            behavior="height"
-                        >
-                            <View style={styles.containerIzquierdo}>
-                                <Text style={styles.letraTitulo} >Ingrese sus credenciales</Text>
-                            </View>
-                            <View style={styles.inputContainer}>
-                                <Text style={styles.letra} >Correo:</Text>
-                                <TextInput
-                                    placeholder="Correo"
-                                    value={email}
-                                    onChangeText={text => setEmail(text)}
-                                    style={styles.input}
-                                />
+  return (
+    <>
+      <ContenedorPrincipal
+        titulo="Iniciar Sesión"
+        navigation={navigation}
+        contenido={
+          <>
+            <View style={styles.cabeceraMensaje}>
+              <Text style={styles.letraTitulo}>Complete los campos</Text>
+            </View>
+            <View style={styles.imgContainer}>
+              <FontAwesomeIcon icon={faImage} size={iconSize} />
+            </View>
 
-                            </View>
+            <KeyboardAvoidingView style={styles.container} behavior="height">
+              <View style={styles.containerIzquierdo}>
+                <Text style={styles.letraTitulo}>Ingrese sus credenciales</Text>
+              </View>
+              <View style={styles.inputContainer}>
+                <Text style={styles.letra}>Correo:</Text>
+                <TextInput
+                  placeholder="Correo"
+                  value={email}
+                  onChangeText={(text) => setEmail(text)}
+                  style={styles.input}
+                />
+              </View>
 
-                            <View style={styles.inputContainer}>
-                                <Text style={styles.letra} >Contraseña:</Text>
-                                <TextInput
-                                    placeholder="Contraseña"
-                                    value={password}
-                                    onChangeText={text => setPassword(text)}
-                                    style={styles.input}
-                                    secureTextEntry
-                                />
-                            </View>
-                            <View style={styles.buttonContainer}>
-                                <TouchableOpacity
-                                    onPress={handleLogin}
-                                    style={styles.button}
-                                >
-                                    <Text style={styles.buttonText}>Iniciar Sesión</Text>
-                                </TouchableOpacity>
-                                <TouchableOpacity
-                                    onPress={handleSignUp}
-                                    style={[styles.button]}
-                                >
-                                    <Text style={styles.buttonText}>Crear Cuenta</Text>
-                                </TouchableOpacity>
-                                <TouchableOpacity onPress={handleProvider}>
-                                <FontAwesomeIcon icon={faAddressCard} size={iconSize*0.40} />
-                            </TouchableOpacity>
-                            </View>
-                        </KeyboardAvoidingView>
-
-                    </>
-
-                )}></ContenedorPrincipal>
-        </>
-    );
+              <View style={styles.inputContainer}>
+                <Text style={styles.letra}>Contraseña:</Text>
+                <TextInput
+                  placeholder="Contraseña"
+                  value={password}
+                  onChangeText={(text) => setPassword(text)}
+                  style={styles.input}
+                  secureTextEntry
+                />
+              </View>
+              <View style={styles.buttonContainer}>
+                <TouchableOpacity onPress={handleLogin} style={styles.button}>
+                  <Text style={styles.buttonText}>Iniciar Sesión</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={handleSignUp}
+                  style={[styles.button]}
+                >
+                  <Text style={styles.buttonText}>Crear Cuenta</Text>
+                </TouchableOpacity>
+                <GoogleSigninButton
+                  style={{ width: 200, height: 60 }}
+                  size={GoogleSigninButton.Size.Wide}
+                  color={GoogleSigninButton.Color.Dark}
+                  onPress={handleProvider}
+                />
+              </View>
+            </KeyboardAvoidingView>
+          </>
+        }
+      ></ContenedorPrincipal>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    imgContainer: {
-        marginTop: iconMargin,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    containerIzquierdo: {
-        justifyContent: 'flex-start',
-        padding: 5
-    },
-    inputContainer: {
-        width: '80%',
-        flexDirection: 'row',
-        paddingHorizontal: 15,
-        paddingVertical: 10,
-        borderRadius: 10,
-        marginTop: 5,
-        justifyContent: 'space-evenly',
-    },
-    buttonContainer: {
-        width: '60%',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 20,
-    },
-    button: {
-        backgroundColor: colores.fondoBarras,
-        width: '100%',
-        padding: 5,
-        alignItems: 'center',
-        marginBottom: 10
-    },
-    buttonText: {
-        color: colores.letra,
-        fontSize: 16,
-    },
-    letraTitulo: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        color: colores.letra
-    },
-    letra: {
-        fontSize: 16,
-        color: colores.letra
-    },
-    cabeceraMensaje: {
-        backgroundColor: colores.fondoBarras,
-        height: cabeceraMensajeHeight,
-        padding: cabeceraMensajePadding,
-        marginTop: cabeceraMensajeMargin,
-        alignItems: 'center',
-        alignContent: 'center',
-    },
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imgContainer: {
+    marginTop: iconMargin,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  containerIzquierdo: {
+    justifyContent: "flex-start",
+    padding: 5,
+  },
+  inputContainer: {
+    width: "80%",
+    flexDirection: "row",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+    justifyContent: "space-evenly",
+  },
+  buttonContainer: {
+    width: "60%",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 20,
+  },
+  button: {
+    backgroundColor: colores.fondoBarras,
+    width: "100%",
+    padding: 5,
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: colores.letra,
+    fontSize: 16,
+  },
+  letraTitulo: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: colores.letra,
+  },
+  letra: {
+    fontSize: 16,
+    color: colores.letra,
+  },
+  cabeceraMensaje: {
+    backgroundColor: colores.fondoBarras,
+    height: cabeceraMensajeHeight,
+    padding: cabeceraMensajePadding,
+    marginTop: cabeceraMensajeMargin,
+    alignItems: "center",
+    alignContent: "center",
+  },
+});
 
-export default Login
+export default Login;
