622606fc9522026b26e049f7173ee4aa400c1177
julio <js8215001@gmail.com>
2023-10-23 12:41:04 -0600

    Agregado consumo de api

--- business-app/components/PerfilEmpresa.js
+++ business-app/components/PerfilEmpresa.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ContenedorPrincipal from "./ContenedorPrincipal";
-import { Text, StyleSheet, View, Dimensions, ScrollView } from "react-native";
+import {
+  Text,
+  StyleSheet,
+  View,
+  Dimensions,
+  ScrollView,
+  Image,
+} from "react-native";
 import { colores } from "../config/colores";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faImage } from "@fortawesome/free-solid-svg-icons";
+import axiosInstance from "../config/axios-config";
 
 const screenHeight = Dimensions.get("window").height;
 const cabeceraMensajeHeight = 0.07 * screenHeight;
@@ -12,73 +20,92 @@ const cabeceraMensajeMargin = 0.03 * screenHeight;
 const iconSize = 0.18 * screenHeight;
 const iconMargin = 0.05 * screenHeight;
 
-const PerfilEmpresa = ({route, navigation }) => {
+const PerfilEmpresa = ({ route, navigation }) => {
   const { idEmpresa } = route.params;
+  const [empresa, setEmpresa] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const obtenerInfoEmpresa = async () => {
+      axiosInstance
+        .post("/empresas/info", {
+          id: idEmpresa,
+        })
+        .then(function (response) {
+          setEmpresa(response.data.data);
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        });
+    };
+
+    obtenerInfoEmpresa();
+  }, []);
+
+  useEffect(() => {
+    if (empresa !== null) {
+        // console.log("Empresa:", empresa);
+      setIsLoading(false);
+    }
+  }, [empresa]);
+
   return (
     <ContenedorPrincipal
       titulo="Perfil Empresa"
       navigation={navigation}
       contenido={
         <>
-          <View style={styles.cabeceraMensaje}>
-            <Text style={styles.letraTitulo}>{idEmpresa}</Text>
-          </View>
-          <View style={styles.containerimg}>
-            <ScrollView horizontal={true}>
-              <View style={styles.iconContainer}>
-                <FontAwesomeIcon
-                  icon={faImage}
-                  size={iconSize}
-                  style={styles.iconWithBorder}
-                />
+          {isLoading ? (
+            <Text style={styles.letra}>Cargando...</Text>
+          ) : (
+            <>
+              <View style={styles.cabeceraMensaje}>
+                <Text style={styles.letraTitulo}>{empresa.nombre}</Text>
               </View>
-              <View style={styles.iconContainer}>
-                <FontAwesomeIcon
-                  icon={faImage}
-                  size={iconSize}
-                  style={styles.iconWithBorder}
-                />
-              </View>
-              <View style={styles.iconContainer}>
-                <FontAwesomeIcon
-                  icon={faImage}
-                  size={iconSize}
-                  style={styles.iconWithBorder}
-                />
-              </View>
-              <View style={styles.iconContainer}>
-                <FontAwesomeIcon
-                  icon={faImage}
-                  size={iconSize}
-                  style={styles.iconWithBorder}
-                />
-              </View>
-            </ScrollView>
-          </View>
+              <View style={styles.containerimg}>
+                <ScrollView horizontal={true}>
 
-          <View style={styles.mensajeDetalle}>
-            <Text style={styles.letraInfo}>Detalles</Text>
-          </View>
-          <View style={styles.inputContainer}>
-            <Text style={styles.letra}>
-              Ubicación: Lorem ipsum dolor sit amet
-            </Text>
-            <Text style={styles.letra}>
-              Descripción: Lorem ipsum dolor sit amet
-            </Text>
-            <Text style={styles.letra}>
-              Horario de atención: Lorem ipsum dolor sit amet
-            </Text>
-            <Text style={styles.letra}>
-              Sucursales: Lorem ipsum dolor sit amet
-            </Text>
-            <Text style={styles.letra}>
-              Telefono: Lorem ipsum dolor sit amet
-            </Text>
-            <Text style={styles.letra}>
-              Categoria: Lorem ipsum dolor sit amet
-            </Text>
-          </View>
+                <View style={styles.iconContainer}>
+                    <Image
+                      source={{ uri: `${empresa.imagen.logo.stringValue}` }}
+                      style={styles.cardImage}
+                    />
+                  </View>
+                  {empresa.imagen.otras.arrayValue.values.map((img) => (
+                    <View style={styles.iconContainer} key={img.mapValue.fields.id.integerValue}>
+                      <Image
+                        source={{ uri: `${img.mapValue.fields.url.stringValue}` }}
+                        style={styles.cardImage}
+                      />
+                    </View>
+                  ))}
+                 
+                </ScrollView>
+              </View>
+
+              <View style={styles.mensajeDetalle}>
+                <Text style={styles.letraInfo}>Detalles</Text>
+              </View>
+              <View style={styles.inputContainer}>
+                <Text style={styles.letra}>
+                  Ubicación: 
+                </Text>
+                <Text style={styles.letra}>
+                  Descripción: {empresa.descripcion}
+                </Text>
+                <Text style={styles.letra}>
+                  Horario de atención: Lorem ipsum dolor sit amet
+                </Text>
+                <Text style={styles.letra}>
+                  Sucursales: Lorem ipsum dolor sit amet
+                </Text>
+                <Text style={styles.letra}>
+                  Telefono: Lorem ipsum dolor sit amet
+                </Text>
+              </View>
+            </>
+          )}
         </>
       }
     ></ContenedorPrincipal>
@@ -144,5 +171,10 @@ const styles = StyleSheet.create({
   },
   mensajeDetalle: {
     marginLeft: "10%",
+  },
+  cardImage: {
+    width: iconSize,
+    height: iconSize,
+    borderRadius: 10,
   },
 });
