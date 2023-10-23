b6ba515f6a0caa1597ce69b01636b657da20ecbd
julio <js8215001@gmail.com>
2023-10-22 22:42:17 -0600

    ajuste de interfaz

--- business-app/components/Inicio.js
+++ business-app/components/Inicio.js
@@ -223,6 +223,7 @@ const styles = StyleSheet.create({
     display: "flex",
     justifyContent: "space-evenly",
     flexDirection: "column",
+    width:iconSize*1.25
   },
   iconContainer: {
     marginRight: iconMargin,
