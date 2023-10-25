32e97ac8f110d185496def75f30af50494d3a7fc
Dipaprika <cesar.andres.cubias@gmail.com>
2023-10-24 20:39:58 -0600

    Update ContenedorPrincipal.js
    Agregada navegacion a categorias en menu inferior

--- business-app/components/ContenedorPrincipal.js
+++ business-app/components/ContenedorPrincipal.js
@@ -1,97 +1,92 @@
-import { colores } from "../config/colores"
-import React from 'react'
+import { colores } from "../config/colores";
+import React from "react";
 import {
-    SafeAreaView,
-    StyleSheet,
-    View,
-    Text,
-    Dimensions,
-    TouchableOpacity
-} from 'react-native';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faHouse, faUser, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-
-const screenHeight = Dimensions.get('window').height;
-const iconSize = 0.06 * screenHeight;
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+const screenHeight = Dimensions.get("window").height;
+const iconSize = 0.08 * screenHeight;
 const headerHeight = 0.08 * screenHeight;
 const headerPadding = 0.02 * screenHeight;
-const footerHeight = 0.08 * screenHeight;
-const footerPadding = 0.03 * screenHeight;
+const footerHeight = 0.09 * screenHeight;
+const footerPadding = 0.01 * screenHeight;
 // const screenWidth = Dimensions.get('window').width;
 // console.log(screenWidth);
 // console.log(screenHeight);
 
 const ContenedorPrincipal = (props) => {
+  const { titulo, contenido, navigation } = props;
 
-    const { titulo, contenido, navigation } = props;
+  const IrAInicio = () => {
+    navigation.navigate("Inicio");
+  };
 
-    const IrAInicio = () => {
-        // console.log("Moviendo a Inicio...")
-        navigation.navigate("Inicio")
-    }
-    
-    const IrAIniciarSesion = () => {
-        // console.log("Moviendo a Iniciar Sesion...")
-        navigation.navigate("Login")
-    }
-    return (
-        <>
-            <SafeAreaView style={styles.Cabecera}>
-                <Text style={styles.letra}>{titulo}</Text>
-            </SafeAreaView>
+  const IrACategorias = () => {
+    navigation.navigate("Categorias");
+  };
 
-            <View style={styles.container}>
-                <View style={styles.content}>
-                    {contenido}
-                </View>
-                <View style={styles.footer}>
-                    <TouchableOpacity onPress={IrAInicio}>
-                        <FontAwesomeIcon icon={faHouse} size={iconSize} />
-                    </TouchableOpacity>
-                    <TouchableOpacity >
-                        <FontAwesomeIcon icon={faMagnifyingGlass} size={iconSize} />
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={IrAIniciarSesion}>
-                        <FontAwesomeIcon icon={faUser} size={iconSize} />
-                    </TouchableOpacity>
-                </View>
-            </View>
+  const IrAIniciarSesion = () => {
+    navigation.navigate("Login");
+  };
+  return (
+    <>
+      <SafeAreaView style={styles.Cabecera}>
+        <Text style={styles.letra}>{titulo}</Text>
+      </SafeAreaView>
 
-        </>
-    )
+      <View style={styles.container}>
+        <View style={styles.content}>{contenido}</View>
+        <View style={styles.footer}>
+          <TouchableOpacity onPress={IrAInicio}>
+            <MaterialCommunityIcons name="home" size={iconSize} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={IrACategorias}>
+            <MaterialCommunityIcons name="magnify" size={iconSize} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={IrAIniciarSesion}>
+            <MaterialCommunityIcons name="account" size={iconSize} />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </>
+  );
+};
 
-}
-
-export default ContenedorPrincipal
+export default ContenedorPrincipal;
 
 const styles = StyleSheet.create({
-    Cabecera: {
-        backgroundColor: colores.fondoBarras,
-        height: headerHeight,
-        alignItems: 'center',
-        alignContent: 'center',
-        padding: headerPadding
-    },
-    letra: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        color: colores.letra
-    },
-    footer: {
-        backgroundColor: colores.fondoBarras,
-        height: footerHeight,
-        alignItems: 'center',
-        alignContent: 'center',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        padding: footerPadding,
-        color: colores.letra
-    },
-    container: {
-        flex: 1,
-        backgroundColor: colores.fondo,
-    },
-    content: {
-        flex: 1
-    },
+  Cabecera: {
+    backgroundColor: colores.fondoBarras,
+    height: headerHeight,
+    alignItems: "center",
+    alignContent: "center",
+    padding: headerPadding,
+  },
+  letra: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: colores.letra,
+  },
+  footer: {
+    backgroundColor: colores.fondoBarras,
+    height: footerHeight,
+    alignItems: "center",
+    alignContent: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: footerPadding,
+    color: colores.letra,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: colores.fondo,
+  },
+  content: {
+    flex: 1,
+  },
 });
