ebc2f79a186ca4178fd7b46b52541b90292d7267
julio <js8215001@gmail.com>
2023-10-24 18:41:03 -0600

    ajuste alias colores

--- business-app/config/colores.js
+++ business-app/config/colores.js
@@ -1,8 +1,8 @@
 export const colores = {
     fondo: '#313E50',
     fondoBarras: '#6C6F7F',
-    terciario: '#455561',
-    primario: '#5C6672',
-    primario: '#6C6F7F',
+    primario: '#455561',
+    secundario: '#5C6672',
+    terciario: '#6C6F7F',
     letra: '#FFFFFF',
 }
