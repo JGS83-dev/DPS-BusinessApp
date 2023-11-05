4133bf106cf8300b5d1e61c7ad1dca81eacafc6b
julio <js8215001@gmail.com>
2023-11-04 19:07:33 -0600

    Update CuentasRouter.js

--- business-api/src/rutas/CuentasRouter.js
+++ business-api/src/rutas/CuentasRouter.js
@@ -1,10 +1,8 @@
 import express from 'express';
 import { ActualizarCuenta, CrearCuenta, DesactivarCuenta, InfoCuenta } from './controladores/CuentasController.js';
 const router = express.Router();
-import multer from "multer";
-const upload = multer()
 
-router.post('/crear',upload.single('file'), CrearCuenta);
+router.post('/crear', CrearCuenta);
 router.post('/actualizar', ActualizarCuenta);
 router.post('/eliminar', DesactivarCuenta);
 router.post('/info', InfoCuenta);
